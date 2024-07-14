447643daaa82db7af8b54904848d4520c068f4bb
Rajesh @ BrainUpgrade.in <rajesh@brainupgrade.in>
2024-07-14 17:30:23 +0530

    refine

---
+++ influx/apps/03-kubernetes/telegraf-kubernetes-rbac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# for i in {1..1};do ./telegraf-kubernetes-rbac.sh  mtvlabk8su$i ; done
+namespace=$1
+
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: telegraf-kubernetes-${namespace}
+  namespace: ${namespace}
+subjects:
+- kind: ServiceAccount
+  name: telegraf-kubernetes
+  namespace: ${namespace}
+roleRef:
+  kind: ClusterRole
+  name: telegraf-kubernetes-mtvlabk8s
+  apiGroup: rbac.authorization.k8s.io
+EOF  
