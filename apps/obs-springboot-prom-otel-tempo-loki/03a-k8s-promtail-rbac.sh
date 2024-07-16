ab14046b33a208e85de6f31cda0f557292e67b7d
Rajesh @ BrainUpgrade.in <rajesh@brainupgrade.in>
2024-07-16 19:32:23 +0530

    refine

--- apps/obs-springboot-prom-otel-tempo-loki/03a-k8s-promtail-rbac.sh
+++ apps/obs-springboot-prom-otel-tempo-loki/03a-k8s-promtail-rbac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# for i in {1..1};do ./04-k8s-promtail-rbac.sh  mtvlabk8su$i ; done
+# for i in {1..1};do ./03a-k8s-promtail-rbac.sh  mtvlabk8su$i ; done
 namespace=$1
 
 cat <<EOF | kubectl apply -f -
